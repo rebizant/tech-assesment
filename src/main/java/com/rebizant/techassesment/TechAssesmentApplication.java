9975a5370c8dd9d38422df1e6440b4d97bd63a17
rebizant <noname@noname.com>
2023-01-19 10:25:21 +0100

    init project

---
+++ src/main/java/com/rebizant/techassesment/TechAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.rebizant.techassesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechAssesmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rebizant/techassesment/TechAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebizant.techassesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
